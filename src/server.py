a563b9342e26612902195760aa893b1805db7669
sgujjar <sgujjar@hotmail.com>
2020-10-03 12:57:00 -0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hi, Hello World!"
+    return "Hi Gujjar, Hello World!"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
